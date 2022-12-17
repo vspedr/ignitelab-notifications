8f7a1a0657b386a0305ae6c5b5320184aff4841c
Victor Pedrazzi <vspedrazzi@gmail.com>
2022-12-17 16:13:21 -0300

    chore: update tsconfig and jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,12 @@
-export default {
+import { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
@@ -9,3 +15,5 @@ export default {
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
 };
+
+export default config;
