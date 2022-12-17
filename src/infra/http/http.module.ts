661b2a0df1f0ee2a6baec087cfc9b2dcfad97dda
Victor Pedrazzi <vspedrazzi@gmail.com>
2022-12-17 16:26:08 -0300

    feat(notification): include new use cases in http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
-import { SendNotification } from 'src/app/use-cases/send-notification';
-import { DatabaseModule } from '../database/database.module';
+import { DatabaseModule } from '@infra/database/database.module';
+
+import { SendNotification } from '@app/use-cases/send-notification';
 import { NotificationsController } from './controllers/notifications.controller';
+import { CancelNotification } from '@app/use-cases/cancel-notification';
+import { CountRecipientNotifications } from '@app/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@app/use-cases/get-recipient-notifications';
+import { ReadNotification } from '@app/use-cases/read-notification';
+import { UnreadNotification } from '@app/use-cases/unread-notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}
