a97a8461a5b98bbb3492b9bc3c372e5e94b69c2f
Victor Pedrazzi <vspedrazzi@gmail.com>
2022-12-17 16:19:55 -0300

    feat(notification): add get recipient notifications use case

---
+++ src/app/use-cases/get-recipient-notifications.spec.ts
@@ -0,0 +1,31 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { GetRecipientNotifications } from './get-recipient-notifications';
+
+describe('Get recipient notification', () => {
+  it('returns notifications of a given recipient', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const getRecipientNotifications = new GetRecipientNotifications(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(makeNotification());
+    await notificationsRepository.create(makeNotification());
+    await notificationsRepository.create(
+      makeNotification({
+        recipientId: 'another-recipient-id',
+      }),
+    );
+
+    const { notifications } = await getRecipientNotifications.execute({
+      recipientId: 'example-recipient-id',
+    });
+
+    expect(notifications).toHaveLength(2);
+    expect(notifications).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ recipientId: 'example-recipient-id' }),
+      ]),
+    );
+  });
+});
